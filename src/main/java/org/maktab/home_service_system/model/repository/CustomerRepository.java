c510c71d63d5627e93b5efa4ac47eb1a9c18c92d
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-05-12 20:24:07 +0430

    add CustomerRepository.java

---
+++ src/main/java/org/maktab/home_service_system/model/repository/CustomerRepository.java
@@ -0,0 +1,24 @@
+package org.maktab.home_service_system.model.repository;
+
+
+import org.maktab.home_service_system.model.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    Customer findByUsernameAndPassword(String username,String password);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsCustomersByPassword(String password);
+
+    List<Customer> findAllByFirstnameOrLastnameOrEmail(String firstname , String lastname , String email);
+
+
+
+}
