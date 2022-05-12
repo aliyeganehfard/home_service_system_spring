b8b14c1282c899b630ad940848ec20f9f24bf8b9
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-05-12 20:18:44 +0430

    Initial commit

---
+++ src/test/java/org/maktab/home_service_system/HomeServiceSystemApplicationTests.java
@@ -0,0 +1,19 @@
+package org.maktab.home_service_system;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.maktab.home_service_system.model.entity.Admin;
+import org.maktab.home_service_system.model.service.impl.AdminServiceImpl;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.List;
+
+@SpringBootTest
+class HomeServiceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/org/maktab/home_service_system/model/service/impl/AdminServiceImplTest.java
@@ -0,0 +1,19 @@
+package org.maktab.home_service_system.model.service.impl;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.maktab.home_service_system.model.entity.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminServiceImplTest {
+
+}
