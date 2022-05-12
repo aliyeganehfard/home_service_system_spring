4ba0a8724bf1ade3c0ce540fb6a2d5ef8c480bb8
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-05-12 20:31:08 +0430

    add OrderState.java

---
+++ src/main/java/org/maktab/home_service_system/model/util/OrderState.java
@@ -0,0 +1,10 @@
+package org.maktab.home_service_system.model.util;
+
+public enum OrderState {
+    WAITING_FOR_EXPERT_SUGGESTION,
+    WAITING_FOR_EXPERT_SELECTION,
+    WAITING_FOR_EXPERT_TO_COME,
+    STARTED,
+    DONE,
+    PAID
+}
